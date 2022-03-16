0987d2d7d57fd5866a31c4ece3fc452dfe86fa33
ankitchaudharyy <ankitdelhi20@gmail.com>
2022-03-16 12:41:33 +0530

    filter

---
+++ filter.js
@@ -0,0 +1,24 @@
+// Implementing Higher Order Function - filter()
+
+// filter() definition 
+// Two parameters : array - array of numbers
+//                : func - isOddNumber
+function filter(array, func){
+    var filteredArray = [];
+
+    for(var idx=0; idx<array.length; idx++){
+        if(isOddNumber(array[idx]) == true)
+            filteredArray.push(array[idx]);
+    }
+
+    return filteredArray;
+}
+
+// filter() execution
+var result = filter([1,2,3,4,5], isOddNumber);
+console.log(result);
+
+// Function to be applied.
+function isOddNumber(number){ 
+    return number%2 ;
+}
